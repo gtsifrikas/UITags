c7d663be778cb2350a98617cc453398167bc9bcd
Tsif <gtsifrikas@gmail.com>
2016-05-30 00:16:05 +0300

    Remove fb unit test library

--- Example/Pods/Target Support Files/Pods-UITags_Tests/Pods-UITags_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-UITags_Tests/Pods-UITags_Tests-frameworks.sh
@@ -86,10 +86,8 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/UICollectionViewLeftAlignedLayout/UICollectionViewLeftAlignedLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/UITags/UITags.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/FBSnapshotTestCase/FBSnapshotTestCase.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/UICollectionViewLeftAlignedLayout/UICollectionViewLeftAlignedLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/UITags/UITags.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/FBSnapshotTestCase/FBSnapshotTestCase.framework"
 fi
