6ab8f0ee146c40b04c8e79115b6e9478cddfcfed
Fernando <tato469@gmail.com>
2016-11-22 14:47:36 +0100

    Updated to Swift 3. Added custom colors. Allow to set non-clickable tags.

--- Pod/Classes/UITags.swift
+++ Pod/Classes/UITags.swift
@@ -3,6 +3,7 @@
 //  Pods
 //
 //  Created by George Tsifrikas on 06/12/15.
+//  Edited by Fernando Valle on 22/10/16.
 //
 //
 
@@ -10,49 +11,67 @@ import UIKit
 import UICollectionViewLeftAlignedLayout
 
 @IBDesignable
-public class UITags: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+open class UITags: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    @IBInspectable public var tagColor: UIColor?
-    @IBInspectable public var tagSelectedColor: UIColor?
+    @IBInspectable open var tagColor: UIColor?
+    @IBInspectable open var tagSelectedColor: UIColor?
     
-    @IBInspectable public var fontSize: CGFloat = 11.0
-    @IBInspectable public var fontFamily = "System"
-    @IBInspectable public var textColor: UIColor?
-    @IBInspectable public var textColorSelected: UIColor?
+    @IBInspectable open var fontSize: CGFloat = 11.0
+    @IBInspectable open var fontFamily = "System"
+    @IBInspectable open var textColor: UIColor?
+    @IBInspectable open var textColorSelected: UIColor?
     
-    @IBInspectable public var tagHorizontalDistance: CGFloat = 2
-    @IBInspectable public var tagVerticalDistance: CGFloat = 3
+    @IBInspectable open var tagHorizontalDistance: CGFloat = 2
+    @IBInspectable open var tagVerticalDistance: CGFloat = 3
     
-    @IBInspectable public var horizontalPadding: CGFloat = 3
-    @IBInspectable public var verticalPadding: CGFloat = 2
-    @IBInspectable public var tagCornerRadius: CGFloat = 3
-
+    @IBInspectable open var horizontalPadding: CGFloat = 3
+    @IBInspectable open var verticalPadding: CGFloat = 2
+    @IBInspectable open var tagCornerRadius: CGFloat = 3
     
-    public var collectionView: UICollectionView?
+    @IBInspectable open var canBeSelected = true
     
-    public var layout: UICollectionViewLayout = UICollectionViewLeftAlignedLayout() {
+    
+    
+    open var collectionView: UICollectionView?
+    
+    open var layout: UICollectionViewLayout = UICollectionViewLeftAlignedLayout() {
         didSet {
             collectionView?.removeFromSuperview()
             setUp()
             setNeedsLayout()
         }
     }
-
-    override public func prepareForInterfaceBuilder() {
+    
+    override open func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
         self.tags = ["This", "is","a", "demo","for", "storyboard",".", "Please","make", "an","outlet", "and", "specify", "your", "own", "tags"]
     }
     
-    public var delegate: UITagsViewDelegate?
+    open var delegate: UITagsViewDelegate?
     
-    public var tags: [String] = [] {
+    open var tags: [String] = [] {
         didSet {
             self.selectedTags.removeAll()
             self.createTags()
         }
     }
     
-    public var selectedTags = [Int]()
+    
+    open var customColors: [Int : UIColor] = [:] {
+        didSet {
+            self.selectedTags.removeAll()
+            self.createTags()
+        }
+    }
+    
+    open var customSelectedColors: [Int : UIColor] = [:] {
+        didSet {
+            self.selectedTags.removeAll()
+            self.createTags()
+        }
+    }
+    
+    open var selectedTags = [Int]()
     
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -64,33 +83,33 @@ public class UITags: UIView, UICollectionViewDataSource, UICollectionViewDelegat
         setUp()
     }
     
-    private var contentHeight: CGFloat = 0.0
+    fileprivate var contentHeight: CGFloat = 0.0
     
-    private func setUp() {
+    fileprivate func setUp() {
         collectionView = UICollectionView(frame: self.bounds, collectionViewLayout: layout)
         collectionView?.delegate = self
         collectionView?.dataSource = self
         collectionView?.showsHorizontalScrollIndicator = false
         collectionView?.showsVerticalScrollIndicator = false
-        collectionView?.backgroundColor = UIColor.clearColor()
-        collectionView?.registerClass(UITagCollectionViewCell.self, forCellWithReuseIdentifier: "tagCell")
+        collectionView?.backgroundColor = UIColor.clear
+        collectionView?.register(UITagCollectionViewCell.self, forCellWithReuseIdentifier: "tagCell")
         if let collectionView = collectionView {
             self.addSubview(collectionView)
             self.layoutSubviews()
         }
     }
     
-    private func createTags() {
+    fileprivate func createTags() {
         collectionView?.reloadData()
         layoutSubviews()
     }
     
-    public override func intrinsicContentSize() -> CGSize {
+    open override var intrinsicContentSize : CGSize {
         let size = CGSize(width: frame.width, height: contentHeight)
         return size
     }
     
-    override public func layoutSubviews() {
+    override open func layoutSubviews() {
         super.layoutSubviews()
         collectionView?.frame = bounds
         collectionView?.layoutSubviews()
@@ -99,15 +118,15 @@ public class UITags: UIView, UICollectionViewDataSource, UICollectionViewDelegat
     }
     
     //MARK: - collection view dataSource implemantation
-    public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    open func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return tags.count
     }
     
-    public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        return configureCell(collectionView.dequeueReusableCellWithReuseIdentifier("tagCell", forIndexPath: indexPath), cellForItemAtIndexPath: indexPath)
+    open func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return configureCell(collectionView.dequeueReusableCell(withReuseIdentifier: "tagCell", for: indexPath), cellForItemAtIndexPath: indexPath)
     }
     
-    private func configureCell(cell: UICollectionViewCell, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    fileprivate func configureCell(_ cell: UICollectionViewCell, cellForItemAtIndexPath indexPath: IndexPath) -> UICollectionViewCell {
         guard let cellToConfigure = cell as? UITagCollectionViewCell else {
             print("Could not load UITagCollectionViewCell..")
             return UICollectionViewCell()
@@ -116,37 +135,45 @@ public class UITags: UIView, UICollectionViewDataSource, UICollectionViewDelegat
         cellToConfigure.fontFamily = self.fontFamily
         cellToConfigure.fontSize = self.fontSize
         cellToConfigure.textColor = self.selectedTags.contains(indexPath.row) ? self.textColorSelected : self.textColor
-        cellToConfigure.contentView.backgroundColor = self.selectedTags.contains(indexPath.row) ? self.tagSelectedColor : self.tagColor
+        if(self.customColors[indexPath.row] != nil)
+        {
+            cellToConfigure.contentView.backgroundColor = self.selectedTags.contains(indexPath.row) ?
+                self.customSelectedColors[indexPath.row] : self.customColors[indexPath.row]
+        }
+        else{
+            cellToConfigure.contentView.backgroundColor = self.selectedTags.contains(indexPath.row) ? self.tagSelectedColor : self.tagColor
+        }
+        
         cellToConfigure.title = self.tags[indexPath.row]
         return cellToConfigure
     }
     
     //MARK: - util methods
-    private func sizeForCellAt(indexPath:NSIndexPath) -> CGSize {
+    fileprivate func sizeForCellAt(_ indexPath:IndexPath) -> CGSize {
         let tempLabel = UILabel()
         tempLabel.text = self.tags[indexPath.row]
         tempLabel.font = UIFont(name: fontFamily, size: fontSize)
         tempLabel.textColor = textColor
-        tempLabel.textAlignment = .Center
+        tempLabel.textAlignment = .center
         
-        var size = tempLabel.sizeThatFits(CGSize(width: CGFloat.max, height: CGFloat.max))
+        var size = tempLabel.sizeThatFits(CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude))
         size.height += 2 * verticalPadding
         size.width += 2 * horizontalPadding
         
         return size
     }
     
-    private func calculatedHeight() -> CGFloat {
+    fileprivate func calculatedHeight() -> CGFloat {
         var totalHeight: CGFloat = 0
         
-        let numberOfTags = collectionView!.numberOfItemsInSection(0)
+        let numberOfTags = collectionView!.numberOfItems(inSection: 0)
         
         let maximumRowWidth = frame.size.width
         
         var currentRowWidth: CGFloat = 0.0
         for var tagIndex in 0..<numberOfTags {
             
-            var cellSize = sizeForCellAt(NSIndexPath(forItem: tagIndex, inSection: 0))
+            var cellSize = sizeForCellAt(IndexPath(item: tagIndex, section: 0))
             cellSize.height += tagVerticalDistance
             cellSize.width += tagHorizontalDistance
             
@@ -166,45 +193,57 @@ public class UITags: UIView, UICollectionViewDataSource, UICollectionViewDelegat
     }
     
     //MARK: - collectionview flow layout delegate implementation
-    public func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return sizeForCellAt(indexPath)
     }
     
-    public func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return self.tagHorizontalDistance
     }
     
-    public func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return self.tagVerticalDistance
     }
     
-    public func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-        return UIEdgeInsetsZero
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
     }
     
     //MARK: - collection view delegate implementation
     
-    public func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        if self.selectedTags.contains(indexPath.row) {
-            self.selectedTags.removeObject(indexPath.row)
-            self.delegate?.tagDeselected(atIndex: indexPath.row)
-        } else {
-            self.selectedTags += [indexPath.row]
-            self.delegate?.tagSelected(atIndex: indexPath.row)
+    open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if(canBeSelected)
+        {
+            if self.selectedTags.contains(indexPath.row) {
+                self.selectedTags.removeObject(indexPath.row)
+                self.delegate?.tagDeselected(atIndex: indexPath.row)
+            } else {
+                self.selectedTags += [indexPath.row]
+                self.delegate?.tagSelected(atIndex: indexPath.row)
+            }
+            
+            self.configureCell(self.collectionView!.cellForItem(at: indexPath)!, cellForItemAtIndexPath: indexPath)
         }
         
-        self.configureCell(self.collectionView!.cellForItemAtIndexPath(indexPath)!, cellForItemAtIndexPath: indexPath)
+    }
+    
+    open func setCustomColor(color: UIColor, selectedColor: UIColor, position: Int)
+    {
+        self.customColors[position] = color
+        self.customSelectedColors[position] = selectedColor != nil ? selectedColor : color
+        self.selectedTags.removeAll()
+        self.createTags()
     }
 }
 
 private extension Array where Element: Equatable {
-    mutating func removeObject(object: Element) {
-        if let index = self.indexOf(object) {
-            self.removeAtIndex(index)
+    mutating func removeObject(_ object: Element) {
+        if let index = self.index(of: object) {
+            self.remove(at: index)
         }
     }
     
-    mutating func removeObjectsInArray(array: [Element]) {
+    mutating func removeObjectsInArray(_ array: [Element]) {
         for object in array {
             self.removeObject(object)
         }
